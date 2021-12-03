44953ed94d92055273ad0c096e4befdc6a0f3388
Hector <hector@Hectors-iMac.local>
2021-12-03 13:42:49 -0600

    add auht login logout

--- 01-starting-project/src/components/Auth.js
+++ 01-starting-project/src/components/Auth.js
@@ -1,10 +1,17 @@
 import classes from './Auth.module.css';
-
+import { useSelector, useDispatch } from 'react-redux';
+import { authActions } from '../store/index'
 const Auth = () => {
+  // const auth = useSelector(state => state.auth);
+  const dispatch = useDispatch();
+  const submitHandler = (event) => {
+    dispatch(authActions.login())
+    event.preventDefault();
+  }
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={submitHandler}>
           <div className={classes.control}>
             <label htmlFor='email'>Email</label>
             <input type='email' id='email' />
